e5a81fb7f48bbc540981325dbd6f144ba221494f
dipanda <dipanda@paypal.com>
2017-03-03 18:21:30 +0530

    Added log4j logging

--- src/main/java/com/github/dibyaranjan/asynclogger/App.java
+++ src/main/java/com/github/dibyaranjan/asynclogger/App.java
@@ -2,13 +2,23 @@ package com.github.dibyaranjan.asynclogger;
 
 import java.io.IOException;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.github.dibyaranjan.asynclogger.spring.loader.ApplicationContextLoader;
 
 public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String[] args) throws IOException {
-        ApplicationContextLoader contextLoader = new ApplicationContextLoader();
-        contextLoader.loadApplicationContext();
-        AppRunner runner = (AppRunner) contextLoader.getBean(AppRunner.class);
-        runner.start();
+        try {
+            ApplicationContextLoader contextLoader = new ApplicationContextLoader();
+            contextLoader.loadApplicationContext();
+            AppRunner runner = (AppRunner) contextLoader.getBean(AppRunner.class);
+            runner.start();
+        } catch (Exception e) {
+            logger.error(ExceptionUtils.getFullStackTrace(e));
+        }
     }
 }
